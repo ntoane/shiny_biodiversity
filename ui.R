e30d30f532b743a79b51e4c2ea38a9b8c68dd4e9
Lekuba Ntoane <l.ntoane@gmail.com>
2023-01-16 09:39:02 +0200

    Added Home tab body content

--- ui.R
+++ ui.R
@@ -11,7 +11,12 @@ sidebar <- dashboardSidebar(
 body <- dashboardBody(
   tabItems(
     tabItem(tabName = "home",
-            h3("Dashboard works")
+            h4("Welcome to Poland Biodiversity"),
+            p("This dashboard app allows you to visualize the selected Poland species on the map and how often it is observed. 
+              You can view the species listed on the table by clicking on \"Species on Table\" on the side bar menu. To visualize the 
+              species on the map, click \"Species on Map\" on the side bar menu. You will then be able to search for species by thier 
+              vernacular nane and scientific name for the app to display observations on the map. You should be able to view 
+              a visualization of a timeline when selected species were observed")
     ),
     
     tabItem(tabName = "table",
@@ -24,7 +29,7 @@ body <- dashboardBody(
   )
 )
 
-ui <- dashboardPage(skin = "green",
+ui <- dashboardPage(skin = "green", title = "Appsilon | Poland Biodiversity",
   dashboardHeader(title = tags$b("Poland Biodiversity")),
   sidebar,
   body
