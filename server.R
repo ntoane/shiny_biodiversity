2c89ae4d92e8dea7e46889a5b5996421ce97b24c
Lekuba Ntoane <l.ntoane@gmail.com>
2023-01-19 17:16:26 +0200

    Added leaflet map to display selected species

--- server.R
+++ server.R
@@ -10,9 +10,8 @@ server <- function(input, output, session) {
   output$speciesTable <- renderDataTable({
     datatable(table_data())
   })
-  
-  # Visualization on map
-  # Get data
+
+  # Get whole data
   get_data <- reactive({
     get_data <- data
   })
@@ -21,10 +20,44 @@ server <- function(input, output, session) {
   output$select_species <- renderUI(
     selectizeInput(inputId = "species_name", 
                label = "Select Scientific or Vernacular Name",
-               selected = c("Alces alces", "Cygnus olor"),
+               selected = c("Alces alces", "Silene viscaria"),
                choices = c(get_data()["scientificName"], get_data()["vernacularName"]),
                multiple = TRUE
     )
     
   )
+  
+  # Filter the data by scientificName or vernacularName
+  filter_data <- eventReactive(input$species_name, {
+    validate(
+      need(input$species_name, "Please select Vernacular name or Scientific name to visualize them on the map here")
+    )
+    
+    filter_data <- dplyr::filter(get_data(),get_data()$scientificName %in% input$species_name | 
+                                   get_data()$vernacularName %in% input$species_name)
+  }, ignoreNULL = FALSE)
+  
+  # Visualization of species on the map
+  output$leafletMap <- renderLeaflet({
+    #input_data
+    filter_data <- filter_data()
+  
+    # Leaflet popup features
+    filter_data$popup_features <- with(filter_data, paste(
+      "<p> <b>Vernacular name:</b>", vernacularName, " </br>",
+      "<b>Scientific name:</b>", scientificName, "</br>",
+      "<b>Kingdom:</b>", kingdom, "</br>",
+      "<b>Family:</b>", family, "</br>",
+      "<b>Sex:</b>", sex, "</br>",
+      "<b>Locality:</b>", locality, "</br>",
+      "<b>Event Date:</b>", eventDate, "</br>",
+      "<b>Event Time:</b>", eventTime, "</br>",
+      "</p>"))
+    
+    leaflet(filter_data) %>%
+     addTiles() %>%
+    addMarkers(lat = filter_data$latitudeDecimal, 
+               lng = filter_data$longitudeDecimal,
+               popup = filter_data$popup_features)
+  })
 }
